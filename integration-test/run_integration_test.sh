a3ce1946445595f40365111ea5ffd37a67811d76
Jacopo <jacopo.carlini@gmail.com>
2022-09-21 18:07:37 +0200

    integration test

---
+++ integration-test/run_integration_test.sh
@@ -0,0 +1,28 @@
+# run from this directory
+
+# azure login
+az acr login -n pagopaucommonacr
+
+# create containers
+docker-compose up -d
+
+# waiting the containers
+printf 'Waiting for the service'
+attempt_counter=0
+max_attempts=10
+until $(curl --output /dev/null --silent --head --fail http://localhost:8080/info); do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "Max attempts reached"
+      exit 1
+    fi
+
+    printf '.'
+    attempt_counter=$(($attempt_counter+1))
+    sleep 5
+done
+printf 'Service Started'
+
+# run integration tests
+cd src || exit
+yarn install
+yarn test
