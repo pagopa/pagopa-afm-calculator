58720ae5fcdd81fe0a528bdf557561746713edb7
Jacopo <jacopo.carlini@gmail.com>
2022-09-22 10:39:12 +0200

    env variable

--- integration-test/run_integration_test.sh
+++ integration-test/run_integration_test.sh
@@ -6,7 +6,7 @@ docker-compose up -d
 # waiting the containers
 printf 'Waiting for the service'
 attempt_counter=0
-max_attempts=10
+max_attempts=50
 until $(curl --output /dev/null --silent --head --fail http://localhost:8080/info); do
     if [ ${attempt_counter} -eq ${max_attempts} ];then
       echo "Max attempts reached"
@@ -15,7 +15,7 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:8080/inf
 
     printf '.'
     attempt_counter=$(($attempt_counter+1))
-    sleep 5
+    sleep 6
 done
 printf 'Service Started'
 
