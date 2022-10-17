7f9b2a56c6a09d2e1694ba10165bfd4ffbd37717
Jacopo Carlini <jacopo.carlini@gmail.com>
2022-10-17 11:58:58 +0200

    feat: Docker Compose (#28)
    * docker compose
    
    * fix scripts

--- integration-test/run_integration_test.sh
+++ integration-test/run_integration_test.sh
@@ -1,10 +1,11 @@
-# run from this directory
-# use -local for run docker-compose-local.yml
-# example: sh ./run_integration_test.sh -local
-cd ..
+# example: sh ./run_integration_test.sh <local|dev|uat|prod>
+if [[ "$(pwd)" =~ .*"integration-test" ]]; then
+  cd ..
+fi
 
 # create containers
-docker-compose -f ./docker-compose$1.yml up -d --remove-orphans --force-recreate
+cd ./docker || exit
+sh ./run_docker.sh "$1"
 
 # waiting the containers
 printf 'Waiting for the service'
@@ -17,12 +18,12 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:8080/inf
     fi
 
     printf '.'
-    attempt_counter=$(($attempt_counter+1))
+    attempt_counter=$((attempt_counter+1))
     sleep 6
 done
 echo 'Service Started'
 
 # run integration tests
-cd integration-test/src || exit
+cd ../integration-test/src || exit
 yarn install
 yarn test
