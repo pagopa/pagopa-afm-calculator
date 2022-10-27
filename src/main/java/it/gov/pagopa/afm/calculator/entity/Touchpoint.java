a3ffa4ea1e32d336f9d25e69d87b7f361cb8c2ce
MarcoAGreco <marcoandreagreco@gmail.com>
2022-10-27 11:46:26 +0200

    [PGEC-84] feat: add touchpoints entity

---
+++ src/main/java/it/gov/pagopa/afm/calculator/entity/Touchpoint.java
@@ -0,0 +1,33 @@
+package it.gov.pagopa.afm.calculator.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import javax.persistence.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+@Table(name = "TOUCHPOINT", schema = "AFM_CALCULATOR")
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class Touchpoint {
+    @Id
+    private String id;
+
+    private String name;
+
+    private LocalDateTime createdDate;
+}
