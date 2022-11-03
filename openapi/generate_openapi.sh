22b759a47fe99b98f0f362554b69d31c4365f45d
Jacopo <jacopo.carlini@gmail.com>
2022-11-03 15:40:10 +0100

    fix

--- openapi/generate_openapi.sh
+++ openapi/generate_openapi.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# create containers
-cd ../docker || exit
-sh ./run_docker.sh "$1"
-
-cd ../openapi || exit
 curl http://localhost:8080/v3/api-docs | python3 -m json.tool > ./openapi.json
 python3 generate_openapi_node.py ./openapi.json
 
-# UI mode http://localhost:8585/swagger-ui/index.html
+# UI mode http://localhost:8080/swagger-ui/index.html
