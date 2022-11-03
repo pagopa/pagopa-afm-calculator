35e518861471eac76e2f73ecdcf7d4cc70f0e288
Jacopo <jacopo.carlini@gmail.com>
2022-11-03 14:46:20 +0100

    check

--- openapi/generate_openapi.sh
+++ openapi/generate_openapi.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# create containers
+cd ../docker || exit
+sh ./run_docker.sh "$1"
+
+cd ../openapi || exit
 curl http://localhost:8080/v3/api-docs | python3 -m json.tool > ./openapi.json
 python3 generate_openapi_node.py ./openapi.json
 
