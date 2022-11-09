908a1ff5bc526a2b2e5bf53d4129f08d1cbb873c
MarcoAGreco <marcoandreagreco@gmail.com>
2022-11-09 11:24:20 +0100

    [PGEC-84] fix: add tests, increase coverage

--- src/test/java/it/gov/pagopa/afm/calculator/service/CalculatorServiceTest.java
+++ src/test/java/it/gov/pagopa/afm/calculator/service/CalculatorServiceTest.java
@@ -5,6 +5,7 @@ import com.azure.spring.data.cosmos.core.query.CosmosQuery;
 import it.gov.pagopa.afm.calculator.TestUtil;
 import it.gov.pagopa.afm.calculator.entity.Touchpoint;
 import it.gov.pagopa.afm.calculator.entity.ValidBundle;
+import it.gov.pagopa.afm.calculator.exception.AppException;
 import it.gov.pagopa.afm.calculator.model.PaymentOption;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
@@ -13,11 +14,16 @@ import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.testcontainers.shaded.org.bouncycastle.est.LimitedSource;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -79,6 +85,39 @@ class CalculatorServiceTest {
 
         String actual = TestUtil.toJson(result);
         String expected = TestUtil.readStringFromFile("responses/getFees_noInTransfer.json");
+        JSONAssert.assertEquals(expected, actual, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    void calculate_invalidTouchpoint() throws IOException, JSONException {
+        Touchpoint touchpoint = TestUtil.getMockTouchpoints();
+
+        when(cosmosTemplate.find(any(CosmosQuery.class), any(), anyString())).thenReturn(
+                Collections.emptyList(), Collections.singleton(TestUtil.getMockValidBundle()));
+
+        var paymentOption = TestUtil.readObjectFromFile("requests/getFees.json", PaymentOption.class);
+
+        AppException exception = assertThrows(AppException.class, () ->
+                calculatorService.calculate(paymentOption, 10));
+
+        assertEquals(HttpStatus.NOT_FOUND, exception.getHttpStatus());
+    }
+
+    @Test
+    void calculateWithIDpsps() throws IOException, JSONException {
+        String expected = TestUtil.readStringFromFile("responses/getFees.json");
+        Touchpoint touchpoint = TestUtil.getMockTouchpoints();
+
+        when(cosmosTemplate.find(any(CosmosQuery.class), any(), anyString())).thenReturn(
+                Collections.singleton(touchpoint), Collections.singleton(TestUtil.getMockValidBundle()));
+
+        var paymentOption = TestUtil.readObjectFromFile("requests/getFees.json", PaymentOption.class);
+        paymentOption.setIdPspList(List.of("PSP"));
+
+        var result = calculatorService.calculate(paymentOption, 10);
+
+        String actual = TestUtil.toJson(result);
+
         JSONAssert.assertEquals(expected, actual, JSONCompareMode.STRICT);
     }
 }
