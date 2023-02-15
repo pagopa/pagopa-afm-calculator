bb03ff04092054bf9b327c82cbdf0ddaf73f9767
aacitelli <aacitelli@PDD-NB-0205.dgsgroup.it>
2023-02-15 16:41:40 +0100

    [PAGOPA-709] flag threshold: added belowThreshold field swagger description

--- src/main/java/it/gov/pagopa/afm/calculator/model/calculator/BundleOption.java
+++ src/main/java/it/gov/pagopa/afm/calculator/model/calculator/BundleOption.java
@@ -2,6 +2,8 @@ package it.gov.pagopa.afm.calculator.model.calculator;
 
 import java.io.Serializable;
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +17,7 @@ public class BundleOption implements Serializable {
   /** generated serialVersionUID */
   private static final long serialVersionUID = -7404184031676587394L;
 
+  @Schema(description = "if true (the payment amount is lower than the threshold value) the bundles onus is not calculated (always false)")
   private Boolean belowThreshold;
   private List<Transfer> bundleOptions;
 }
