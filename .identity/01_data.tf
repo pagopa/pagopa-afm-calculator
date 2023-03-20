9b4c45b6c13163d08ee9e4bf5df4fbf88b87fe7b
Jacopo <jacopo.carlini@gmail.com>
2023-03-20 11:38:19 +0100

    deploy

--- .identity/01_data.tf
+++ .identity/01_data.tf
@@ -12,3 +12,24 @@ data "azurerm_api_management_product" "product" {
   api_management_name = "${local.product}-apim"
   resource_group_name = "${local.product}-api-rg"
 }
+
+data "azurerm_key_vault" "key_vault" {
+  count  = var.env_short == "d" ? 1 : 0
+
+  name = "pagopa-${var.env_short}-kv"
+  resource_group_name = "pagopa-${var.env_short}-sec-rg"
+}
+
+data "azurerm_key_vault_secret" "key_vault_sonar" {
+  count  = var.env_short == "d" ? 1 : 0
+
+  name = "sonar-token"
+  key_vault_id = data.azurerm_key_vault.key_vault[0].id
+}
+
+data "azurerm_key_vault_secret" "key_vault_bot_token" {
+  count  = var.env_short == "d" ? 1 : 0
+
+  name = "bot-token-github"
+  key_vault_id = data.azurerm_key_vault.key_vault[0].id
+}
