ffb54fcd04512a4d57857415dca7140816518de9
Jacopo <jacopo.carlini@gmail.com>
2023-05-10 17:13:05 +0200

    [PAGOPA-923] fix bot token

--- openapi/generate_openapi.sh
+++ openapi/generate_openapi.sh
@@ -5,16 +5,8 @@ if [ $(npm list -g | grep -c api-spec-converter) -eq 0 ]; then
   npm install -g api-spec-converter
 fi
 
-if ! $(curl --output /dev/null --silent --head --fail http://localhost:8080/actuator/info); then
-  # create containers
-  cd ../docker || exit
-  sh ./run_docker.sh "$1" "$2"
-  cd ../openapi || exit
-fi
-
-
 # save openapi
-curl http://localhost:8080/v3/api-docs> ./openapi.json
+curl http://localhost:8080/v3/api-docs | python3 -m json.tool > ./openapi.json
 python3 generate_openapi_node.py ./openapi.json
 
 # UI mode http://localhost:8080/swagger-ui/index.html
