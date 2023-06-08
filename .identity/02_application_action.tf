4348dae9ded80aea62088a2f6841c43915298e40
Jacopo <jacopo.carlini@gmail.com>
2023-06-08 17:01:44 +0200

    fix

--- .identity/02_application_action.tf
+++ .identity/02_application_action.tf
@@ -51,14 +51,14 @@ resource "azurerm_role_assignment" "environment_terraform_resource_group_dashboa
 resource "azurerm_role_assignment" "environment_key_vault" {
   scope                = data.azurerm_key_vault.key_vault.id
   role_definition_name = "Reader"
-  principal_id         = module.github_runner_app.client_id
+  principal_id         = module.github_runner_app.object_id
 }
 
 resource "azurerm_key_vault_access_policy" "ad_group_policy" {
   key_vault_id = data.azurerm_key_vault.key_vault.id
 
   tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = module.github_runner_app.client_id
+  object_id = module.github_runner_app.object_id
 
   key_permissions         = ["Get", "List", "Import"]
   secret_permissions      = ["Get", "List"]
