ee4a16741c04de15d4ca9bc63a6ca89411664b88
FedericoRuzzier <49512050+FedericoRuzzier@users.noreply.github.com>
2024-03-21 09:33:57 +0100

    PAGOPA-1529 testing new scenario

--- performance-test/src/calculator_getFeesMulti.js
+++ performance-test/src/calculator_getFeesMulti.js
@@ -66,9 +66,9 @@ export default function calculator() {
 
     // to give randomness to request in order to avoid caching
     const paymentAmount = Math.floor(Math.random() * (100 + __VU) % 100);
-    //const primaryCreditorInstitution1 = '7777777777' + Math.floor(Math.random() * 10);
-    //const primaryCreditorInstitution2 = '7777777777' + Math.floor(Math.random() * 10);
-    const primaryCreditorInstitution = 'fiscalCode-' + Math.floor(Math.random() * 2) + 1;
+    const primaryCreditorInstitution1 = '7777777777' + Math.floor(Math.random() * 10);
+    const primaryCreditorInstitution2 = '7777777777' + Math.floor(Math.random() * 10);
+    //const primaryCreditorInstitution = 'fiscalCode-' + Math.floor(Math.random() * 2) + 1;
 
     let payload = {
         "bin": "1005066",
@@ -77,7 +77,7 @@ export default function calculator() {
         "idPspList": [],
         "paymentNotice": [
             {
-                "primaryCreditorInstitution": primaryCreditorInstitution,
+                "primaryCreditorInstitution": primaryCreditorInstitution1,
                 "paymentAmount": paymentAmount/2,
                 "transferList": [
                     {
@@ -87,7 +87,7 @@ export default function calculator() {
                 ]
             },
             {
-                "primaryCreditorInstitution": primaryCreditorInstitution,
+                "primaryCreditorInstitution": primaryCreditorInstitution2,
                 "paymentAmount": paymentAmount/2,
                 "transferList": [
                     {
