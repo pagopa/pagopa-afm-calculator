7da5b5b72e63a1611a5b35f57b9c86552fa837ef
Federico Ruzzier <federicoruzzier96@gmail.com>
2025-03-20 11:05:19 +0100

    fix: generate openapi

--- openapi/generate_openapi.sh
+++ openapi/generate_openapi.sh
@@ -5,3 +5,6 @@ if [[ "$(pwd)" =~ .*"openapi".* ]]; then
 fi
 
 mvn test -Dtest=OpenApiGenerationTest
+
+jq '.paths."/fees" = .paths."/fees/multi" | del( .paths."/fees/multi")' ./openapi/openapi-v2.json > ./openapi/temp.json.temp && mv ./openapi/temp.json.temp ./openapi/openapi-v2.json
+jq '.paths."/psps/fees/{idPsp}" = .paths."/psps/{idPsp}/fees/multi" | del( .paths."/psps/{idPsp}/fees/multi")' ./openapi/openapi-v2.json > ./openapi/temp.json.temp && mv ./openapi/temp.json.temp ./openapi/openapi-v2.json
