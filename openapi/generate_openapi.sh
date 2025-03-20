2aa0f3ce7a30959c11fe55f66cb02a2df786fe10
Federico Ruzzier <federicoruzzier96@gmail.com>
2025-03-20 11:08:10 +0100

    fix: generate openapi node

--- openapi/generate_openapi.sh
+++ openapi/generate_openapi.sh
@@ -8,3 +8,6 @@ mvn test -Dtest=OpenApiGenerationTest
 
 jq '.paths."/fees" = .paths."/fees/multi" | del( .paths."/fees/multi")' ./openapi/openapi-v2.json > ./openapi/temp.json.temp && mv ./openapi/temp.json.temp ./openapi/openapi-v2.json
 jq '.paths."/psps/fees/{idPsp}" = .paths."/psps/{idPsp}/fees/multi" | del( .paths."/psps/{idPsp}/fees/multi")' ./openapi/openapi-v2.json > ./openapi/temp.json.temp && mv ./openapi/temp.json.temp ./openapi/openapi-v2.json
+
+jq '.paths."/fees" = .paths."/fees/multi" | del( .paths."/fees/multi")' ./openapi/openapi-node-v2.json > ./openapi/temp.json.temp && mv ./openapi/temp.json.temp ./openapi/openapi-node-v2.json
+jq '.paths."/psps/fees/{idPsp}" = .paths."/psps/{idPsp}/fees/multi" | del( .paths."/psps/{idPsp}/fees/multi")' ./openapi/openapi-node-v2.json > ./openapi/temp.json.temp && mv ./openapi/temp.json.temp ./openapi/openapi-node-v2.json
