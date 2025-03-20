927f8a65db60e58b1fe62d778c160c7dee1a1f0b
Federico Ruzzier <federicoruzzier96@gmail.com>
2025-03-20 11:11:41 +0100

    fix: typo in script

--- openapi/generate_openapi.sh
+++ openapi/generate_openapi.sh
@@ -7,7 +7,7 @@ fi
 mvn test -Dtest=OpenApiGenerationTest
 
 jq '.paths."/fees" = .paths."/fees/multi" | del( .paths."/fees/multi")' ./openapi/openapi-v2.json > ./openapi/temp.json.temp && mv ./openapi/temp.json.temp ./openapi/openapi-v2.json
-jq '.paths."/psps/fees/{idPsp}" = .paths."/psps/{idPsp}/fees/multi" | del( .paths."/psps/{idPsp}/fees/multi")' ./openapi/openapi-v2.json > ./openapi/temp.json.temp && mv ./openapi/temp.json.temp ./openapi/openapi-v2.json
+jq '.paths."/psps/{idPsp}/fees" = .paths."/psps/{idPsp}/fees/multi" | del( .paths."/psps/{idPsp}/fees/multi")' ./openapi/openapi-v2.json > ./openapi/temp.json.temp && mv ./openapi/temp.json.temp ./openapi/openapi-v2.json
 
 jq '.paths."/fees" = .paths."/fees/multi" | del( .paths."/fees/multi")' ./openapi/openapi-node-v2.json > ./openapi/temp.json.temp && mv ./openapi/temp.json.temp ./openapi/openapi-node-v2.json
-jq '.paths."/psps/fees/{idPsp}" = .paths."/psps/{idPsp}/fees/multi" | del( .paths."/psps/{idPsp}/fees/multi")' ./openapi/openapi-node-v2.json > ./openapi/temp.json.temp && mv ./openapi/temp.json.temp ./openapi/openapi-node-v2.json
+jq '.paths."/psps/{idPsp}/fees" = .paths."/psps/{idPsp}/fees/multi" | del( .paths."/psps/{idPsp}/fees/multi")' ./openapi/openapi-node-v2.json > ./openapi/temp.json.temp && mv ./openapi/temp.json.temp ./openapi/openapi-node-v2.json
