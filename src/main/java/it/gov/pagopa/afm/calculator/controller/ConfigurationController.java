08ba288d368a5c341f9edea8f3c38bc0e791bedd
svariant <samuele.varianti@nttdata.com>
2025-07-30 15:09:30 +0200

    feat: [PAGOPA-3210] API to refresh cache issuer range table

--- src/main/java/it/gov/pagopa/afm/calculator/controller/ConfigurationController.java
+++ src/main/java/it/gov/pagopa/afm/calculator/controller/ConfigurationController.java
@@ -1,12 +1,20 @@
 package it.gov.pagopa.afm.calculator.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import it.gov.pagopa.afm.calculator.entity.PaymentType;
 import it.gov.pagopa.afm.calculator.entity.Touchpoint;
 import it.gov.pagopa.afm.calculator.entity.ValidBundle;
+import it.gov.pagopa.afm.calculator.model.ProblemJson;
 import it.gov.pagopa.afm.calculator.service.ConfigurationService;
+import it.gov.pagopa.afm.calculator.service.IssuersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +30,8 @@ public class ConfigurationController {
 
     @Autowired
     private ConfigurationService configurationService;
+    @Autowired
+    private IssuersService issuersService;
 
     @PostMapping("/bundles/add")
     public ResponseEntity<Void> addValidBundles(@RequestBody List<ValidBundle> validBundles) {
@@ -56,6 +66,35 @@ public class ConfigurationController {
     @PostMapping("/paymenttypes/delete")
     public ResponseEntity<Void> deletePaymentTypes(@RequestBody List<PaymentType> paymentTypes) {
         configurationService.deletePaymentTypes(paymentTypes);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "Refresh issuer range table cache")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Ok",
+                            content = @Content(schema = @Schema())),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "Unauthorized",
+                            content = @Content(schema = @Schema())),
+                    @ApiResponse(
+                            responseCode = "429",
+                            description = "Too many requests",
+                            content = @Content(schema = @Schema())),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Service unavailable",
+                            content =
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ProblemJson.class)))
+            })
+    @PostMapping("/refresh/issuers")
+    public ResponseEntity<Void> refreshIssuerRangeTableCache() {
+        issuersService.getIssuerRangeTableCached();
         return ResponseEntity.ok().build();
     }
 }
