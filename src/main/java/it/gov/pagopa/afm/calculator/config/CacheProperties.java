7b1558c2a4434bd783cebfb9c3b588606fbf14fe
Jacopo Carlini <jacopo.carlini@pagopa.it>
2025-08-01 16:18:36 +0200

    fix

---
+++ src/main/java/it/gov/pagopa/afm/calculator/config/CacheProperties.java
@@ -0,0 +1,18 @@
+package it.gov.pagopa.afm.calculator.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+@ConfigurationProperties(prefix = "cache")
+@Getter
+@Setter
+public class CacheProperties {
+    private Map<String, Long> ttl;
+    private Map<String, Long> maxSize;
+
+}
