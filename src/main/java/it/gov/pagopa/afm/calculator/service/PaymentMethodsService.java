975bc316c7d6feb54abc03d519cc243b5a93ca53
Jacopo Carlini <jacopo.carlini@pagopa.it>
2025-09-17 10:47:52 +0200

    fix allCCp

--- src/main/java/it/gov/pagopa/afm/calculator/service/PaymentMethodsService.java
+++ src/main/java/it/gov/pagopa/afm/calculator/service/PaymentMethodsService.java
@@ -41,7 +41,7 @@ public class PaymentMethodsService {
                             .idPspList(null)
                             .paymentNotice(request.getPaymentNotice())
                             .build(),
-                    Integer.MAX_VALUE, request.getAllCCp(), false, "fee");
+                    Integer.MAX_VALUE, Boolean.TRUE.equals(request.getAllCCp()), false, "fee");
 
             // filter by bundles
             FeeRange feeRange = null;
