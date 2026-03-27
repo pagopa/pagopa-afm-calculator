003e30823158b5f4ad6e52352a09f5e8d8253a74
Forciniti Luigi <forciniti@spi.srl>
2026-03-27 17:38:49 +0100

    chore:remove unused code in calculator_getFeesMulti.js

--- performance-test/src/calculator_getFeesMulti.js
+++ performance-test/src/calculator_getFeesMulti.js
@@ -3,8 +3,7 @@
 
 import {check} from 'k6';
 import {SharedArray} from 'k6/data';
-import {addTouchpoints, deleteTouchpoints, getFeesMulti, mapToValidBundles, getValidBundle} from './helpers/calculator_helper.js';
-import { createDocument, deleteDocument } from "./helpers/cosmosdb_client.js";
+import {getFeesMulti} from './helpers/calculator_helper.js';
 
 export let options = JSON.parse(open(__ENV.TEST_TYPE));
 
@@ -15,35 +14,10 @@ const varsArray = new SharedArray('vars', function () {
     return JSON.parse(open(`${__ENV.VARS}`)).environment;
 });
 
-const data = JSON.parse(open('./helpers/data.json'));
-const touchpoints = data["touchpoints"];
-const paymenttypes = data["paymenttypes"];
-
 // workaround to use shared array (only array should be used)
 const vars = varsArray[0];
 const rootUrl = `${vars.hostV2}`;
 const cartPathApi = `${vars.cartPathApi}`;
-const cosmosDBURI = `${vars.cosmosDBURI}`;
-const databaseID = `${vars.databaseID}`;
-const validBundlesNum = `${vars.validBundlesNum}`;
-
-const cosmosPrimaryKey = `${__ENV.COSMOS_SUBSCRIPTION_KEY}`;
-
-
-export function setup() {
-    // 2. setup code (once)
-    // The setup code runs, setting up the test environment (optional) and generating data
-    // used to reuse code for the same VU
-
-//    for (let i = 0; i < validBundlesNum; i++) {
-//        let validBundle = getValidBundle("int-test-"+i);
-//        let response = createDocument(cosmosDBURI, databaseID, "validbundles", cosmosPrimaryKey, validBundle, validBundle['idPsp']);
-//        check(response, { "status is 201": (res) => (res.status === 201) });
-//    }
-
-    // precondition is moved to default fn because in this stage
-    // __VU is always 0 and cannot be used to create env properly
-}
 
 export default function calculator() {
 
@@ -94,14 +68,4 @@ export default function calculator() {
     check(response, {
         'getFeesMulti': (r) => r.status === 200,
     });
-
-}
-
-export function teardown() {
-
-//    for (let i = 0; i < validBundlesNum; i++) {
-//        let validBundle = getValidBundle("int-test-"+i);
-//        let response = deleteDocument(cosmosDBURI, databaseID, "validbundles", cosmosPrimaryKey, validBundle['id'], validBundle['idPsp']);
-//        check(response, { "status is 204": (res) => (res.status === 204) });
-//    }
 }
